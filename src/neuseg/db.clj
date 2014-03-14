c74c96bc8ddfcf24067ffcab8997b77645f719c4
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-14 19:57:22 +0800

    refactor

--- src/neuseg/db.clj
+++ src/neuseg/db.clj
@@ -16,23 +16,28 @@
         nvec  (normalise (vec (map #(Double. %1) (rest items))))]
         (tuple wd nvec)))
 
-(defn- reg-wd-neglect [name data]
+(defn- process-line [vecmodel name data]
   (let [[[wd nvec] idx] data]
       (do
-        (if (= (mod idx 10000) 0) (print "."))
+        (if (= (mod idx 1000) 0) (print "."))
         (swap! (get registry name) assoc wd idx)
-        nvec)))
+        (set-row! vecmodel idx nvec))))
 
 (defn load-db [name]
+  (println "")
+  (println "loading" name "started!")
   (with-open [rdr (clojure.java.io/reader (str "data" "/" name))]
-    (do
-      (println "")
-      (println "loading" name "started!")
-      (let [last (atom -1)]
-        (matrix (map (partial reg-wd-neglect name)
-             (partition-by #(if (number? %1) (reset! last %1) (+ @last 1))
-               (interleave (map splited-line (rest (line-seq rdr)))
-                           (iterate inc 0)))))))))
+    (let [data  (line-seq rdr)
+          declr (first data)
+          [size dim] (map #(Integer. %) (clojure.string/split line #"\s+"))
+          last (atom -1)
+          vecmodel (new-matrix size dim)]
+          (dorun
+            (map (partial process-line vecmodel name)
+              (partition-by #(if (number? %1) (reset! last %1) (+ @last 1))
+                (interleave (map splited-line data)
+                            (iterate inc 0)))))
+          vecmodel)))
 
 (def unigram  (load-db "unigram"))
 (def bigram   (load-db "bigram"))
