1ee308b14031c4155ef927fbaaa0a65872853d9b
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-17 17:42:49 +0800

    typo

--- java/com/guokr/neuseg/util/NGram.java
+++ java/com/guokr/neuseg/util/NGram.java
@@ -4,34 +4,34 @@ import java.util.Iterator;
 
 public class NGram {
 
-	public static Iterator<CharSequence> ngram(int n, String line)
+	public static Iterator<CharSequence> ngram(int n, String text)
 			throws Exception {
 		NGramIterator iter = new NGramIterator(n);
-		iter.reset(line);
+		iter.reset(text);
 		return iter;
 	}
 
-	public static Iterator<CharSequence> unigram(String line) throws Exception {
+	public static Iterator<CharSequence> unigram(String text) throws Exception {
 		NGramIterator iter = new NGramIterator(1);
-		iter.reset(line);
+		iter.reset(text);
 		return iter;
 	}
 
-	public static Iterator<CharSequence> bigram(String line) throws Exception {
+	public static Iterator<CharSequence> bigram(String text) throws Exception {
 		NGramIterator iter = new NGramIterator(2);
-		iter.reset(line);
+		iter.reset(text);
 		return iter;
 	}
 
-	public static Iterator<CharSequence> trigram(String line) throws Exception {
+	public static Iterator<CharSequence> trigram(String text) throws Exception {
 		NGramIterator iter = new NGramIterator(3);
-		iter.reset(line);
+		iter.reset(text);
 		return iter;
 	}
 
-	public static Iterator<CharSequence> quadgram(String line) throws Exception {
+	public static Iterator<CharSequence> quadgram(String text) throws Exception {
 		NGramIterator iter = new NGramIterator(4);
-		iter.reset(line);
+		iter.reset(text);
 		return iter;
 	}
 
