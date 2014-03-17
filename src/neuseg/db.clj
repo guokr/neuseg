6e39332782a00437159d305999e80ea9d05d3a7c
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-17 19:26:59 +0800

    fix

--- src/neuseg/db.clj
+++ src/neuseg/db.clj
@@ -1,15 +1,22 @@
 (ns neuseg.db
   (:use [clj-tuple]
         [clojure.core.matrix :only [set-current-implementation
-                                    new-matrix set-row!]]))
+                                    new-matrix set-row! get-row
+                                    zero-vector normalise]]))
 
 (set-current-implementation :vectorz)
 
-(def registry
+(def indexes
   { "unigram"   (atom {})
     "bigram"    (atom {})
     "trigram"   (atom {})
     "quadgram"  (atom {}) })
+
+(def dimensions
+  { "unigram"   (atom 0)
+    "bigram"    (atom 0)
+    "trigram"   (atom 0)
+    "quadgram"  (atom 0) })
 
 (defn- splited-line [line]
   (let [items (clojure.string/split line #"\s+")
@@ -20,18 +27,18 @@
 (defn- process-line [vecmodel name data]
   (let [[[wd nvec] idx] data]
       (do
-        (swap! (get registry name) assoc wd idx)
+        (swap! (get indexes name) assoc wd idx)
         (set-row! vecmodel idx nvec))))
 
 (defn load-db [name]
-  (println "")
-  (println "loading" name "started!")
+  (println "loading" name "started......")
   (with-open [rdr (clojure.java.io/reader (str "data" "/" name))]
     (let [data  (line-seq rdr)
           declr (first data)
           [size dim] (map #(Integer. %) (clojure.string/split declr #"\s+"))
           last (atom -1)
           vecmodel (new-matrix size dim)]
+          (reset! (get dimensions name) dim)
           (dorun
             (map (partial process-line vecmodel name)
               (partition-by #(if (number? %1) (reset! last %1) (+ @last 1))
@@ -43,3 +50,23 @@
 (def bigram   (load-db "bigram"))
 (def trigram  (load-db "trigram"))
 (def quadgram (load-db "quadgram"))
+
+(def ngrams { "unigram" unigram
+              "bigram" bigram
+              "trigram" trigram
+              "quadgram" quadgram })
+
+(defn- retrieve-vector [name s]
+  (let [idx (get @(get indexes name) s)
+        dim @(get dimensions name)
+        ngram (get ngrams name)]
+    (if idx (get-row ngram idx) (zero-vector dim))))
+
+(defn get-vector [s]
+  (case (.length s)
+    1 (retrieve-vector "unigram" s)
+    2 (retrieve-vector "bigram" s)
+    3 (retrieve-vector "trigram" s)
+    4 (retrieve-vector "quadgram" s)
+    (zero-vector 0)))
+
--- src/neuseg/train.clj
+++ src/neuseg/train.clj
@@ -1,16 +1,23 @@
 (ns neuseg.train
   (:use [clj-tuple]
-        [clojure.core.matrix :only [dot]]
+        [clojure.core.matrix :only [dot zero-vector]]
         [neuseg.db]
         [com.guokr.nlp.seg]
         [clojure.string :only [join]])
   (:import (com.guokr.neuseg.util NGram
                                   NeighborSlider)))
 
-(defn ndot [vecs n]
+(defn- ndot [vecs n]
   (let [mid (/ (dec n) 2)
         mid-vec (nth vecs mid)]
     (map (partial dot mid-vec) (concat (take mid vecs) (drop (inc mid) vecs)))))
+
+(defn- zip [& colls]
+  (partition (count colls) (apply interleave colls)))
+
+(defn- tformat [coll]
+  (let [row (flatten coll)]
+    (str (join " " (take 32 row) "\n" (join " " (nthrest 32 row)) "\n"))))
 
 (defn tagging [text]
   (let [seged (seg text)]
@@ -22,16 +29,9 @@
           (recur (rest raw) (rest sgd) (conj ret -1 -1))
           (recur (rest raw) (nthrest sgd 2) (conj ret 1 1)))))))
 
-(defn zip [& colls]
-  (partition (count colls) (apply interleave colls)))
-
-(defn format [coll]
-  (let [row (flattern coll)]
-    (str (join " " (take 32 row) "\n" (join " " (nthrest 32 row)) "\n"))))
-
 (defn gen-case [text]
-  (map format (zip (map ndot (NeighborSlider. 4 (zero-vector 16) (iter-seq (NGram/unigram text))))
-                   (map ndot (NeighborSlider. 4 (zero-vector 16) (iter-seq (NGram/bigram text))))
-                   (map ndot (NeighborSlider. 4 (zero-vector 16) (iter-seq (NGram/trigram text))))
-                   (map ndot (NeighborSlider. 4 (zero-vector 16) (iter-seq (NGram/quadgram text))))
-                   (partition 2 (tagging text)))))
+  (map tformat (zip (map ndot (NeighborSlider. 4 (get-vector " ") (map get-vector (iterator-seq (NGram/unigram text)))))
+                    (map ndot (NeighborSlider. 4 (get-vector "  ") (map get-vector (iterator-seq (NGram/bigram text)))))
+                    (map ndot (NeighborSlider. 4 (get-vector "   ") (map get-vector (iterator-seq (NGram/trigram text)))))
+                    (map ndot (NeighborSlider. 4 (get-vector "    ") (map get-vector (iterator-seq (NGram/quadgram text)))))
+                    (partition 2 (tagging text)))))
