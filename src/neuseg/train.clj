18563520bcdd9088c56b5739537c716b4b62c57d
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-19 19:16:45 +0800

    add sainity test

--- src/neuseg/train.clj
+++ src/neuseg/train.clj
@@ -2,11 +2,21 @@
   (:require [neuseg.fann :as fann]))
 
 (defn train []
-  (let [nn         (fann/create [32 64 16 2] :hidden :sigmoid-symmetric :output :threshold-symmetric)
+  (let [nn         (fann/create [32 64 16 2] :hidden :sigmoid-symmetric :output :sigmoid-symmetric)
         train-data (fann/load-train-data "data/trains/train")
         test-data  (fann/load-train-data "data/trains/tests")]
     (println "training...")
-    (fann/train nn train-data 500 1 0.001)
+    (fann/train nn train-data 100 1 0.001)
+    (println "testing...")
+    (println "error = " (fann/testnn nn test-data))
+    (fann/save nn "data/models/tagging.nn")))
+
+(defn sainity-test []
+  (let [nn         (fann/create [2 3 3 1] :hidden :sigmoid-symmetric :output :sigmoid-symmetric)
+        train-data (fann/load-train-data "data/trains/xor")
+        test-data  (fann/load-train-data "data/trains/xor")]
+    (println "training...")
+    (fann/train nn train-data 1000 1 0.001)
     (println "testing...")
     (println "error = " (fann/testnn nn test-data))
     (fann/save nn "data/models/tagging.nn")))
