5e5a26c76698841d8c6d4bbf3e8ffff4ed046639
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-19 18:39:00 +0800

    fix train

--- src/neuseg/train.clj
+++ src/neuseg/train.clj
@@ -2,11 +2,11 @@
   (:require [neuseg.fann :as fann]))
 
 (defn train []
-  (let [nn         (fann/create [32 64 2] :hidden :sigmoid-symmetric :output :threshold-symmetric)
+  (let [nn         (fann/create [32 64 16 2] :hidden :sigmoid-symmetric :output :threshold-symmetric)
         train-data (fann/load-train-data "data/trains/train")
         test-data  (fann/load-train-data "data/trains/tests")]
     (println "training...")
-    (fann/train nn train-data 100 1 0.001)
+    (fann/train nn train-data 500 1 0.001)
     (println "testing...")
     (println "error = " (fann/testnn nn test-data))
     (fann/save nn "data/models/tagging.nn")))
