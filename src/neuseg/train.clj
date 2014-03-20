dc2bba47fa1d40358fb17259d0e525233fb37d32
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-20 18:45:52 +0800

    fix exit condition

--- src/neuseg/train.clj
+++ src/neuseg/train.clj
@@ -6,7 +6,7 @@
         train-data (fann/load-train-data "data/trains/train")
         test-data  (fann/load-train-data "data/trains/tests")]
     (println "training...")
-    (fann/train nn train-data 100 1 0.001)
+    (fann/train nn train-data 1000 1 0.001)
     (println "testing...")
     (println "error = " (fann/testnn nn test-data))
     (fann/save nn "data/models/tagging.nn")))
