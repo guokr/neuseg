702043a26a838d3b793db7ac1dd1ce10a04b76a3
Mingli Yuan <mingli.yuan@gmail.com>
2014-03-21 22:08:08 +0800

    fix arity

--- src/neuseg/seg.clj
+++ src/neuseg/seg.clj
@@ -6,14 +6,15 @@
 
 (def tagging (fann/mk-invoke-fn nn 2))
 
-(defn untagging [tag ch]
-  (if (>= (first tag)  0)
-    (if (>= (second tag)  0)
-      (str " " ch " ")
-      (str " " ch))
-    (if (>= (second tag)  0)
-      (str ch " ")
-      (str ch))))
+(defn untagging [pair]
+  (let [[tag ch] pair]
+    (if (>= (first tag)  0)
+      (if (>= (second tag)  0)
+        (str " " ch " ")
+        (str " " ch))
+      (if (>= (second tag)  0)
+        (str ch " ")
+        (str ch)))))
 
 (defn seg [text]
   (map untagging (zip (map tagging (vectorize text)) (vec text))))
